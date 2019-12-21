21b65293795a7ead8dd32f8cdf28081010cf8e48
Yunus <karazakuulu.54@gmail.com>
2019-12-21 22:16:04 +0100

    To do list has been created

--- Django-Projects/todo/views.py
+++ Django-Projects/todo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from .models import TodoItem
 
 def todoView(request):
@@ -7,4 +7,12 @@ def todoView(request):
     return render(request, 'todo.html',
         {'all_items' : all_todo_items})
 
-# Create your views here.
+def addTodo(request):
+    new_item = TodoItem(content = request.POST['content'])
+    new_item.save()
+    return HttpResponseRedirect('/todo/')
+
+def deleteTodo(request, todo_id):
+    item_to_delete = TodoItem.objects.get(id = todo_id)
+    item_to_delete.delete()
+    return HttpResponseRedirect("/todo/")
